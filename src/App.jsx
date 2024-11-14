f1975ac319dffc291a1374d8e37bf0788e5bd91b
RiteshRay <DeveloperRiteshRay@gmail.com>
2024-11-14 20:19:16 +0530

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+      <div className=' flex flex-col justify-center items-center'>
+        <h1 className=' underline'>Resturent website</h1>
+        <h5 className=' mt-5'>Under mantance</h5>
+       
+      </div>
+  )
+}
+
+export default App
